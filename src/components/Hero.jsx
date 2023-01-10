afd86fee2bbf87cc25102f2f14e4c6ac7c7d7362
abixy <70246562+abixy@users.noreply.github.com>
2023-01-09 21:51:53 -0500

    dsd

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ export const Hero = () => {
         <motion.div
           initial={{ opacity: 0, y: 10 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5 }}
+          transition={{ duration: 3.0 }}
         >
           <div className="text-customSecondary text-sm sm:text-base  mb-6 sm:mt-32 mt-16  font-bold">
             Woods Work, yo.
